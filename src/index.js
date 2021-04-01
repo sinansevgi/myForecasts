ba80f9bf6d311d670933aac92f3fdcaf514681d2
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-04-01 12:37:56 +0300

    Add toggle button functionality
    -Add screenshot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import getForecast from "./modules/dataflow";
-const result = getForecast('gelibolu','metric');
-console.log(result.then(console.log));
+import getForecast from './modules/dataflow';
+import domFuncs from './modules/domFuncs';
+
+const getButton = document.getElementById('getForecast');
+const unitSwitch = document.getElementById('unitSwitch');
+
+getButton.addEventListener('click', () => {
+  const units = domFuncs.unitSelect();
+  const result = getForecast(domFuncs.getCity(), units.isDegree);
+  result
+    .then(
+      data => {
+        if (typeof data === 'string') {
+          domFuncs.printError(data);
+        } else {
+          domFuncs.updateWeather(data, units.unit);
+        }
+      },
+    );
+});
+
+unitSwitch.addEventListener('click', domFuncs.toggleUnit);
--- src/modules/dataflow.js
+++ src/modules/dataflow.js
@@ -1,14 +1,16 @@
 const getForecast = async (city, isDegree) => {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${isDegree}&appid=e32142723756b55b8ac542878b62709a`);
   const data = await response.json();
-  if (response.ok){
-    let forecast = data.weather[0].main;
-    let temperature = data.main.temp;
-    let forecastIcon = data.weather[0].icon;
-    return {forecast, temperature, forecastIcon}
-  } else {
-    return (data.message);
+  if (response.ok) {
+    const forecast = data.weather[0].main;
+    const temperature = data.main.temp;
+    const forecastIcon = data.weather[0].icon;
+    const city = data.name;
+    return {
+      forecast, temperature, forecastIcon, city,
+    };
   }
+  return (data.message);
 };
 
 export { getForecast as default };
